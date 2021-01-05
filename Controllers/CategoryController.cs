670ef518644bf2c91697089106bdacde729f4f55
Ross Baker <rossbaker32@gmail.com>
2021-01-06 10:53:25 +1100

    Add category controller and views

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rocky.Models;
+using Rocky.Data;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
