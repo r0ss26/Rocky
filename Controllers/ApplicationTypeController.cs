d6b4a5a2dc3cb21db59561354f20e7181b059e98
Ross Baker <rossbaker32@gmail.com>
2021-01-07 13:27:37 +1100

    Add Application Type MV&C

--- Controllers/ApplicationTypeController.cs
+++ Controllers/ApplicationTypeController.cs
@@ -10,34 +10,31 @@ using Rocky.Data;
 
 namespace Rocky.Controllers
 {
-    public class CategoryController : Controller
+    public class ApplicationTypeController : Controller
     {
         private readonly ApplicationDbContext _db;
-
-        public CategoryController(ApplicationDbContext db)
+        public ApplicationTypeController(ApplicationDbContext db)
         {
             _db = db;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Category> objList = _db.Category;
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
             return View(objList);
         }
 
         // GET - CREATE
         public IActionResult Create()
         {
-
             return View();
         }
 
-           // POST - CREATE
+        // POST - CREATE
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
+        public IActionResult Create(ApplicationType obj)
         {
-            _db.Category.Add(obj);
+            _db.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
