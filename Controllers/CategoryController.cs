581dc1afa8001aa9fc918978bfba1bc0e8ebe0b4
Ross Baker <rossbaker32@gmail.com>
2021-01-18 17:36:50 +1100

    Edit get and post routes

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -28,18 +28,51 @@ namespace Rocky.Controllers
         // GET - CREATE
         public IActionResult Create()
         {
-
             return View();
         }
 
-           // POST - CREATE
+        // POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
